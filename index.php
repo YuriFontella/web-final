f53c0ddf75de187d24c3c8bb61781bcd69f61a18
Yuri Fontella <yurifc4@gmail.com>
2014-11-27 13:24:40 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bem Vindo!</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/style.css">
+    
+  </head>
+  <body>
+    
+    <div class="top menu">
+      <p>news</p>
+      <img src="img/world.gif" width="30" height="30">
+    </div>
+    <div class="container">
+      <div class="painel fixo">
+        <img src="img/f1.png">
+        <h1>Impressora 3D pode começar a ser vendida no segundo semestre de 2014</h1>
+        <div class="outdoor">
+          <p>A Cube chegou ao Brasil em abril, trazida pela Robtec em parceria com a 3D Systems. Até o mês passado, a impressora só podia ser adquirida na sede da Robtec, em São Paulo, mas agora pode ser comprada em todo o Brasil por meio do e-commerce da Saraiva. A impressora tem entrada USB, com conectividade Wi-Fi, funciona em modo Touch Screen e já vem com um cartucho de PLA e um pen drive de 2GB. Nos últimos anos, as impressoras 3D tornaram-se financeiramente acessíveis para pequenas e médias empresas.</p>
+        </div>
+      </div>
+      <div class="painel news">
+        <div class="media">
+          <a class="media-left" href="#">
+            <img src="img/i1.jpg" class="img-thumbnail">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">Celulares antigos vendidos em toda a europa</h4>
+            Considerado o primeiro celular vendido no Brasil, o aparelho apareceu nas prateleiras em 1990.
+          </div>
+        </div>
+        
+        <div class="media">
+          <a class="media-left" href="#">
+            <img src="img/i2.gif" class="img-thumbnail">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">O mundo está em guerra consatante</h4>
+            Pedimos perdão a deus por tanta violência Lutando pela paz no mundo e agradecendo sua paciência.
+          </div>
+        </div>
+        
+        <div class="media">
+          <a class="media-left" href="#">
+            <img src="img/i3.jpg" class="img-thumbnail">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">O apagão energético de 2001</h4>
+            Em 2001, a população brasileira foi obrigada a mudar seus hábitos de consumo de energia drasticamente.
+          </div>
+        </div>
+        
+        <div class="media">
+          <a class="media-left" href="#">
+            <img src="img/i4.gif" class="img-thumbnail">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">Domingo chuvoso é marcado por acidentes</h4>
+            Duas vítimas ficaram presas as ferragens. O Corpo de Bombeiros foi acionado.
+          </div>
+        </div>
+        
+        <div class="media">
+          <a class="media-left" href="#">
+            <img src="img/i5.jpg" class="img-thumbnail">
+          </a>
+          <div class="media-body">
+            <h4 class="media-heading">Como funciona a Citrix Receiver</h4>
+            O Receiver é um componente essencial da sua infraestrutura de entrega Citrix. 
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="bottom menu">
+      <a href="#">Sobre</a>
+      <a href="#">Ajuda</a>
+      <a href="#">Contato</a>
+    </div>
+
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="assets/script.js"></script>
+  </body>
+</html>
